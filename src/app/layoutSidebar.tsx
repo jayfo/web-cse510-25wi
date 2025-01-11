6b3978ca3d560569e0d0e4c60033e949af8fa873
James Fogarty <jayfo@users.noreply.github.com>
2025-01-11 12:12:32 -0800

    Code Format

--- src/app/layoutSidebar.tsx
+++ src/app/layoutSidebar.tsx
@@ -24,14 +24,14 @@ export function LayoutSidebar({}: LayoutSidebarProps) {
         if (routeSegments.length >= 2) {
           if (routeSegments[1] === "project") {
             return PAGE_LINKS_PROJECT;
-          // } else if (routeSegments[1] === "milestone-2") {
-          //   return PAGE_LINKS_MILESTONE_2;
-          // } else if (routeSegments[1] === "milestone-3") {
-          //   return PAGE_LINKS_MILESTONE_3;
-          // } else if (routeSegments[1] === "milestone-4") {
-          //   return PAGE_LINKS_MILESTONE_4;
-          // } else if (routeSegments[1] === "milestone-5") {
-          //   return PAGE_LINKS_MILESTONE_5;
+            // } else if (routeSegments[1] === "milestone-2") {
+            //   return PAGE_LINKS_MILESTONE_2;
+            // } else if (routeSegments[1] === "milestone-3") {
+            //   return PAGE_LINKS_MILESTONE_3;
+            // } else if (routeSegments[1] === "milestone-4") {
+            //   return PAGE_LINKS_MILESTONE_4;
+            // } else if (routeSegments[1] === "milestone-5") {
+            //   return PAGE_LINKS_MILESTONE_5;
           }
         }
       }
